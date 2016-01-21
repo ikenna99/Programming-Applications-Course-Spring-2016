e1be97784ee1a8c775e784c9d47dd56271dacf27
ikenna99 <ikenna.onyenze@live.com>
2016-01-21 11:17:30 -0500

    Homework submission

--- Week 1/PlotHW1.py
+++ Week 1/PlotHW1.py
@@ -1,20 +1,32 @@
 #import numpy and matlibplots module
-
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import sin, cos, pi
 
 #generate array of x values from 0 to 6pi
+x = np.linspace(0, 6*pi)
+line, = plt.plot(x, np.sin(x), linewidth=1)
 
 #calculate y values for sin wave
+ysin=sin(x)
 
 #calculate y values for cosine wave
+ycos=cos(x)
+    
+#plot x and y values for both functions
+plt.plot(x,ycos)    
 
-#plot x and y values for both functions 
-
-#set dashed line for sine wave and solide line for cosine wave
+plt.ylabel("y")
+plt.xlabel("x")
 
 #add legend
+plt.legend(("sin(x)", "cos(x)"))
 
+#set dashed line for sine wave and solide line for cosine wave        
+dashes = [10, 5, 100, 5]  
+line.set_dashes(dashes) 
+plt.show()
 
-#leave comment here with your information
-#Name:
-#Date:
-
+#Name: Ikenna Onyenze
+#ID: 02754297
+#Date: 1/19/2016
